ff84245ec78c8160ed00f7b2059270b003352335
Abhinav Mishra <135455825+AbhinavMishra32@users.noreply.github.com>
2023-11-18 11:47:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    if (skillsMemberValue == "") {
+        skills.style.display = "none";
+    } else {
+        skills.style.display = "block";
+    }
+}
